a4d2e22c4d60a163f5d43d9b2a57f17c13600a47
Bassam AL SAEDY <basaidi@gmail.com>
2022-10-03 15:53:16 +0200

    wpm test

--- main.py
+++ main.py
@@ -10,12 +10,23 @@ def start_screen(stdscr):
     stdscr.getkey()
 
 
+def wpm_test(stdscr):
+    target_text = 'Hello world this is some test text for this app!'
+    current_text = []
+
+    stdscr.clear()
+    stdscr.addstr(target_text)
+    stdscr.refresh()
+    stdscr.getkey()
+
+
 def main(stdscr):
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
     curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
 
     start_screen(stdscr)
+    wpm_test(stdscr)
 
 
 wrapper(main)
