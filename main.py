81804c4f53754854949c6068e027e4a2e33c0b16
Bassam AL SAEDY <basaidi@gmail.com>
2022-10-03 13:57:51 +0200

    Adding color

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from curses import wrapper
 
 
 def main(stdscr):
+    curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
+    curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
     stdscr.clear()
     stdscr.addstr('Hello world!')
     stdscr.refresh()
